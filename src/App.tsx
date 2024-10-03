4788b631632911f3e280f26a3f747bc027701bee
Ivandro Neto <ivandroneto1@outlook.com>
2024-10-03 09:48:50 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,64 @@
-import QRCode from 'qrcode'
-import styles from  './App.module.css'
-import {useState } from 'react'
+import QRCode from 'qrcode';
+import styles from './App.module.css';
+import { useState } from 'react';
+
 function App() {
   const [url, setUrl] = useState<string>('');
   const [imageURL, setImageURL] = useState('');
-  const [HasData, setHasData] = useState<boolean>(false);
+  const [hasData, setHasData] = useState<boolean>(false);
   const [error, setError] = useState<string>('');
-  const handleInputData = (event : InputEvent) => {
-    const inputEvent = event.target as HTMLInputElement
-    setUrl(inputEvent.value)
-  }
 
-  const GenerateQR = async (event : SubmitEvent) =>{
-    event.preventDefault();
-    try{
-      const qrFile = await QRCode.toDataURL(url)
+  const handleInputData = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUrl(event.target.value);
+  };
+
+  const generateQR = async () => {
+    try {
+      const qrFile = await QRCode.toDataURL(url);
       setImageURL(qrFile);
-      setHasData(true)
-    }catch (err) {
+      setHasData(true);
+    } catch (err) {
       setError('Error generating QR code: ' + err);
     }
-  }
+  };
 
   return (
     <main className={styles.container}>
       <p>{error}</p>
       <div className={styles.card}>
         <h1>QR.Me</h1>
-        {
-          HasData === false?
+        {hasData === false ? (
           <div className={styles.qrcontainer}>
             <h1>QR.Me</h1>
-          </div>  
-          :
-          <a href={imageURL} className={styles.qrcontainer} download={url.replace('www.', 'QR-').replace('https://', "QR-") +".png"}>
-            <img src={imageURL} alt="QR image"/>
+          </div>
+        ) : (
+          <a
+            href={imageURL}
+            className={styles.qrcontainer}
+            download={url.replace('www.', 'QR-').replace('https://', 'QR-') + '.png'}
+          >
+            <img src={imageURL} alt="QR image" />
           </a>
-        }
+        )}
         <p>Link:</p>
-        <input className={styles.input} name="url" type="text" placeholder='https://ivandroneto.com' onChange={()=>handleInputData}/>
-        <button className={styles.btn} onClick={GenerateQR}>
+        <input
+          className={styles.input}
+          name="url"
+          type="text"
+          placeholder="https://ivandroneto.com"
+          onChange={handleInputData}
+        />
+        <button className={styles.btn} onClick={generateQR}>
           Generate
         </button>
       </div>
       <footer>
-        <p>@Developed by <a href="https://www.ivandroneto.com">Ivandro Neto</a></p>
+        <p>
+          @Developed by <a href="https://www.ivandroneto.com">Ivandro Neto</a>
+        </p>
       </footer>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
